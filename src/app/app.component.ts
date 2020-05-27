5fefaf0b49ea63d183d1a63c41ab7f53e1c3fbb0
Wouter van Nuland <wouter.van.nuland@fourtress.nl>
2020-05-27 20:27:04 +0200

    Cleaned up app component code. Added click listener to Rocket icon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'afstuderen-frontend';
+  counter = 'Loading...';
+
+  onRocketClick() {
+    console.log("Launch!");
+  }
 }
